4ed2b612f94db3faa2781ed8e8f9c963e4da208d
Maksym Rybalko <maxymrybalko2005@gmail.com>
2024-04-12 13:35:16 +0300

    Added PageLayout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,13 @@
-'use client';
+import React from 'react';
 
-import AuthAPI, { RegisterData } from '@/lib/api/auth/AuthAPI';
+import PageLayout from '@/components/common/layout/page-layout/PageLayout';
 
-const Home = () => {
-  const handleSubmit = async (formData: FormData) => {
-    const registerData: RegisterData = {
-      firstName: formData.get('firstName')?.toString(),
-      lastName: formData.get('lastName')?.toString(),
-      phone: formData.get('phone')?.toString(),
-      email: formData.get('email')?.toString(),
-      password: formData.get('password')?.toString(),
-    };
-    console.log(registerData);
-    AuthAPI.register(registerData);
-  };
+const Main = () => {
   return (
-    <form action={handleSubmit}>
-      <input name="firstName" />
-      <input name="lastName" />
-      <input name="phone" />
-      <input name="email" />
-      <input name="password" />
-      <button type="submit"> SUBMIT </button>
-    </form>
+    <>
+      <PageLayout />
+    </>
   );
 };
 
-export default Home;
+export default Main;
---
+++ src/components/common/layout/page-layout/PageLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '@/components/common/layout/header';
+
+const PageLayout = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default PageLayout;
